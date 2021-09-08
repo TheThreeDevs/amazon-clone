52b8d72ad1d43cf82524e6aae6e4f7f73cfe712c
Daniel Leal <leadaniel456@gmail.com>
2021-09-08 17:27:39 -0500

    Added NavBar

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+// import './App.css';
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      Amazon Clone!!
+      <NavBar />
     </div>
   );
 }
---
+++ amazon-clone/src/Components/NavBar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import './NavBar.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+
+function NavBar() {
+  return (
+    <nav className="NavBar">
+      {/* Logo to the left */}
+        <img
+        className="NavBar_Logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="logo"
+        />
+       {/* Search Bar and button */}
+        <div className="NavBarSearch">
+          <input  type="text" className="SearchInput"/>
+          <SearchIcon  className="SearchIcon"/>
+        </div>
+
+        {/* Div Container containing two links and the basket , STILL HAVE TO WRAP THESE INTO LINKS */}
+
+        <div className="NavBarHeader">
+          <div className="NavBarOption">
+            <span className="NavBarOptionOne">Hello,</span>
+            <span className="NavBarOptionTwo">Sign In</span>
+          </div>
+
+          <div className="NavBarOption">
+            <span className="NavBarOptionOne">Returns</span>
+            <span className="NavBarOptionTwo">& Orders</span>
+          </div>
+
+          {/* Basket and the number of items in the cart */}
+          <div className="NavBarOptionBasket">
+            <ShoppingBasketIcon />
+            <span className="NavBarOptionTwo NavBarBasketCount">0</span>
+          </div>
+
+        </div>
+
+    </nav>
+  );
+
+}
+
+export default NavBar;
