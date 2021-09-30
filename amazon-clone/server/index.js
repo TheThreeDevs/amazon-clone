a9a883cad008659da6b193184e085807824f80b1
Emmanuel Garcia <earturoga@gmail.com>
2021-09-30 15:27:05 -0600

    setup package json scripts

--- amazon-clone/server/index.js
+++ amazon-clone/server/index.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
-// const DIST_DIR = path.join(__dirname, '../public');
+const DIST_DIR = path.join(__dirname, '../public');
 
 
+app.use(express.static(DIST_DIR));
 app.use(express.json());
 app.use(express.urlencoded()); //To parse URL-encoded bodies!!
-// app.use(express.static(DIST_DIR));
 
 app.get('/home', (req, res) => {
   console.log('Got a get request home');
