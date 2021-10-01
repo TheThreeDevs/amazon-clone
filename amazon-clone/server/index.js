eddc0b759bf685b210f418f47c55ba13acc10361
Emmanuel Garcia <earturoga@gmail.com>
2021-09-30 22:23:21 -0600

    Server get request check from front-end to endpoint /home

--- amazon-clone/server/index.js
+++ amazon-clone/server/index.js
@@ -3,16 +3,18 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
-const DIST_DIR = path.join(__dirname, '../public');
+const DIST_DIR = path.join(__dirname, '../client/public');
+console.log(DIST_DIR)
 
 
 app.use(express.static(DIST_DIR));
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded()); //To parse URL-encoded bodies!!
 
 app.get('/home', (req, res) => {
-  console.log('Got a get request home');
-  res.send('Hello from the express server');
+  console.log('Got a get request to /home');
+  res.send('Hello from the express server!');
 })
 
 
