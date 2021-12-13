298a2eb91a5d7c57dd507c13514baa9af8d93004
Daniel Leal <leadaniel456@gmail.com>
2021-12-12 19:45:44 -0600

    Added all the pictures on the account cards

--- amazon-clone/client/src/Components/Account.js
+++ amazon-clone/client/src/Components/Account.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import './Account.css';
+import OrdersPic from './Images/Orders.png';
+import SecurityPic from './Images/Security.png';
+import PrimePic from './Images/Prime.png';
+import GiftCardPic from './Images/GiftCard.png';
+import PaymentsPic from './Images/Payments.png';
+import ProfilesPic from './Images/Profiles.png';
+import DigitalPic from './Images/Digital.png';
+import MessagesPic from './Images/Messages.jpg';
+import ArchievedPic from './Images/Archived.png';
+import ListPic from './Images/Lists.png';
 
 function Account () {
   return (
@@ -9,43 +19,81 @@ function Account () {
         {/* Here I need to make four rows with three containers  */}
         <div className='accountRow'>
           {/* Need to create three cards in the row */}
-          <div className='accountCard'>Your Orders
-            {/* image */}
-            {/* paragraph */}
-            <p>Track, return, or buy things again</p>
+          <div className='accountCard'>
+              <img src={OrdersPic} alt='orders' className='cardImg'/>
+            <div className='cardColumn'>
+              <div className='cardTitle'> Your Orders </div>
+              <div className='cardParagraph'>Track, return, or buy things again</div>
+            </div>
           </div>
-          <div className='accountCard'>Login & security
-            <p>Edit login, name, and mobile number</p>
+          <div className='accountCard'>
+              <img src={SecurityPic} alt='orders' className='cardImg'/>
+            <div className='cardColumn'>
+              <div className='cardTitle'> Your Orders </div>
+              <div className='cardParagraph'>Track, return, or buy things again</div>
+            </div>
           </div>
-          <div className='accountCard'>Prime
-            <p>View benefits and payment settings</p>
-          </div>
-        </div>
-        <div className='accountRow'>
-          <div className='accountCard'>Gift cards
-            <p>View balance, redeem, or reload cards</p>
-          </div>
-          <div className='accountCard'>Your Payments
-            <p>Manage payment methods and settings, view all transactions</p>
-          </div>
-          <div className='accountCard'>Your Profiles
-            <p>Manage, add, or remove user profiles for personalized experiences</p>
-          </div>
-        </div>
-        <div className='accountRow'>
-          <div className='accountCard'>Digital Services and Device Support
-            <p>Troubleshoot device issues</p>
-          </div>
-          <div className='accountCard'>Your Messages
-            <p>View messages to and from Amazon, sellers, and buyers</p>
-          </div>
-          <div className='accountCard'>Archived orders
-            <p>View and manage archived orders</p>
+          <div className='accountCard'>
+              <img src={PrimePic} alt='orders' className='cardImg'/>
+            <div className='cardColumn'>
+              <div className='cardTitle'> Your Orders </div>
+              <div className='cardParagraph'>Track, return, or buy things again</div>
+            </div>
           </div>
         </div>
         <div className='accountRow'>
-          <div className='accountCard'>Your Lists
-            <p>View, modify, and share your lists, or create new ones</p>
+        <div className='accountCard'>
+              <img src={GiftCardPic} alt='orders' className='cardImg'/>
+            <div className='cardColumn'>
+              <div className='cardTitle'> Your Orders </div>
+              <div className='cardParagraph'>Track, return, or buy things again</div>
+            </div>
+          </div>
+          <div className='accountCard'>
+              <img src={PaymentsPic} alt='orders' className='cardImg'/>
+            <div className='cardColumn'>
+              <div className='cardTitle'> Your Orders </div>
+              <div className='cardParagraph'>Track, return, or buy things again</div>
+            </div>
+          </div>
+          <div className='accountCard'>
+              <img src={ProfilesPic} alt='orders' className='cardImg'/>
+            <div className='cardColumn'>
+              <div className='cardTitle'> Your Orders </div>
+              <div className='cardParagraph'>Track, return, or buy things again</div>
+            </div>
+          </div>
+        </div>
+        <div className='accountRow'>
+        <div className='accountCard'>
+              <img src={DigitalPic} alt='orders' className='cardImg'/>
+            <div className='cardColumn'>
+              <div className='cardTitle'> Your Orders </div>
+              <div className='cardParagraph'>Track, return, or buy things again</div>
+            </div>
+          </div>
+          <div className='accountCard'>
+              <img src={MessagesPic} alt='orders' className='cardImg'/>
+            <div className='cardColumn'>
+              <div className='cardTitle'> Your Orders </div>
+              <div className='cardParagraph'>Track, return, or buy things again</div>
+            </div>
+          </div>
+          <div className='accountCard'>
+              <img src={ArchievedPic} alt='orders' className='cardImg'/>
+            <div className='cardColumn'>
+              <div className='cardTitle'> Your Orders </div>
+              <div className='cardParagraph'>Track, return, or buy things again</div>
+            </div>
+          </div>
+        </div>
+        <div className='accountRow'>
+        <div className='accountCard'>
+              <img src={ListPic} alt='orders' className='cardImg'/>
+            <div className='cardColumn'>
+              <div className='cardTitle'> Your Orders </div>
+              <div className='cardParagraph'>Track, return, or buy things again</div>
+            </div>
           </div>
         </div>
       </div>
