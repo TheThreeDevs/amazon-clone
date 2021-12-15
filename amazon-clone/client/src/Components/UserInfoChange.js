341940a7573b9dd8c514637971b7560859420a9f
Daniel Leal <leadaniel456@gmail.com>
2021-12-15 16:09:06 -0600

    Added the UserInfoChange component

--- amazon-clone/client/src/Components/UserInfoChange.js
+++ amazon-clone/client/src/Components/UserInfoChange.js
@@ -4,7 +4,66 @@ import './UserInfoChange.css';
 function UserInfoChange () {
   return (
     <div>
-      <h2> Login & security</h2>
+      <h3 className='loginTitle'> Login & security</h3>
+      <div className='updateInfoContainer'>
+        {/* 6 divs for the specific change */}
+        <div className='insideContainer'>
+          <div className='insideCategory'>
+            <b>Name:</b>
+            <div>Daniel</div>
+          </div>
+          <div className='insideButton'>
+            <button className='editButton'>Edit</button>
+          </div>
+        </div>
+        <div className='insideContainer'>
+          <div className='insideCategory'>
+            <b>Email:</b>
+            <div>Daniel</div>
+          </div>
+          <div className='insideButton'>
+            <button className='editButton'>Edit</button>
+          </div>
+        </div>
+        <div className='insideContainer'>
+          <div className='insideCategory'>
+            <b>Mobile Phone Number:</b>
+            {/* <a href='google.com'>Why add a mobile number?</a> */}
+            {/* <div>Why add a mobile number?</div> */}
+          </div>
+          <div className='insideButton'>
+            <button className='editButton'>Add</button>
+          </div>
+        </div>
+        <div className='insideContainer'>
+          <div className='insideCategory'>
+            <b>Password:</b>
+            <div>Daniel</div>
+          </div>
+          <div className='insideButton'>
+            <button className='editButton'>Edit</button>
+          </div>
+        </div>
+        <div className='insideContainer'>
+          <div className='insideCategory'>
+            <b>Two-Step Verification:</b>
+            <div>For extra security, require a one-time password at sign-in</div>
+          </div>
+          <div className='insideButton'>
+            <button className='editButton'>Edit</button>
+          </div>
+        </div>
+        <div className='insideContainer'>
+          <div className='insideCategory'>
+            <b>Secure Your Account:</b>
+            <div>If you think your Amazon account has been compromised, follow these steps to make your account more secure.</div>
+          </div>
+          <div className='insideButton'>
+            <button className='editButton'>Edit</button>
+          </div>
+        </div>
+      </div>
+      <button className='doneButton'>Done</button>
     </div>
   )
 }
