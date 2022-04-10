1672e8de97533341ff8d63ad54b2b9f81a23fe54
Emmanuel Garcia <earturoga@gmail.com>
2022-04-10 00:30:57 -0600

    added grid in products home

--- amazon-clone/client/src/Components/ProductsHome.js
+++ amazon-clone/client/src/Components/ProductsHome.js
@@ -1,42 +1,58 @@
-import './ProductsHome.css';
-import { Link } from 'react-router-dom';
+import "./ProductsHome.css";
+import { Link } from "react-router-dom";
 
-function ProductsHome () {
-    //when they click on the tags it should change the state to send them to productsList with the correct products
+function ProductsHome() {
+  //when they click on the tags it should change the state to send them to productsList with the correct products
   return (
-    <div className='d-flex column justify-content-center productsHome'>
-        <div className='productsCard'>
-          {/* title */}
+      <div className="productsHome">
+        <div className="productCard one">
           <h2>Men's Clothing</h2>
-          <Link to={{pathname: '/products', state: {category: `men's clothing`}}}>
-            {/* image */}
-            <div className='productsImg' />
-            <div className='takeLook'>Take a look</div>
+          <Link
+            className="url"
+            to={{
+              pathname: "/products",
+              state: { category: `men's clothing` },
+            }}
+          >
+            <div className="productsImg" />
+            <div className="takeLook">Take a look</div>
           </Link>
         </div>
-        <div className='productsCard'>
+        <div className="productCard two">
           <h2>Jewelery</h2>
-          <Link to={{pathname: '/products', state: {category: 'jewelery'}}}>
-            <div className='productsImgTwo'/>
-            <div className='takeLook'>Take a look</div>
+          <Link
+            className="url"
+            to={{ pathname: "/products", state: { category: "jewelery" } }}
+          >
+            <div className="productsImgTwo" />
+            <div className="takeLook">Take a look</div>
           </Link>
         </div>
-        <div className='productsCard'>
+        <div className="productCard three">
           <h2>Electronics</h2>
-          <Link to={{pathname: '/products', state: {category: 'electronics'}}}>
-            <div className='productsImgThree'/>
-            <div className='takeLook'>Take a look</div>
+          <Link
+            className="url"
+            to={{ pathname: "/products", state: { category: "electronics" } }}
+          >
+            <div className="productsImgThree" />
+            <div className="takeLook">Take a look</div>
           </Link>
         </div>
-        <div className='productsCard'>
+        <div className="productCard four">
           <h2>Women's Clothing</h2>
-          <Link to={{pathname: '/products', state: {category: `women's clothing`}}}>
-            <div className='productsImgFour'/>
-            <div className='takeLook'>Take a look</div>
+          <Link
+            className="url"
+            to={{
+              pathname: "/products",
+              state: { category: `women's clothing` },
+            }}
+          >
+            <div className="productsImgFour" />
+            <div className="takeLook">Take a look</div>
           </Link>
+        </div>
       </div>
-    </div>
-  )
+  );
 }
 
 export default ProductsHome;
